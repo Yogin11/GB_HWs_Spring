6027becb022c1930be17c2e8e2e1595e1b7bc903
MaxYogin <yogin11@yahoo.com>
2024-02-06 16:24:55 +0300

    Config Encoder update

--- src/main/java/com/maximus/simpleSpringSecurity/configurations/SecurityConfig.java
+++ src/main/java/com/maximus/simpleSpringSecurity/configurations/SecurityConfig.java
@@ -1,10 +1,18 @@
 package com.maximus.simpleSpringSecurity.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -30,9 +38,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
                 auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
+//                .withUser("user").password("{noop}password").roles("USER")
+                .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
                 .and()
                 .withUser("admin").password("{noop}password").roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        Map<String,PasswordEncoder> myEncoders = new HashMap<>();
+        myEncoders.put("bcrypt", new BCryptPasswordEncoder());
+        return new DelegatingPasswordEncoder("bcrypt", myEncoders);
     }
 
 }
